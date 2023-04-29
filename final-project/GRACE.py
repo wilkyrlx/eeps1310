e179a4c7b0d705dd3ff450c8daff65cb1cbbed8e
John Wilkinson <93553222+wilkyrlx@users.noreply.github.com>
2023-04-29 08:00:01 -0400

    changes to GRACE

--- final-project/GRACE.py
+++ final-project/GRACE.py
@@ -6,24 +6,28 @@ import cartopy.feature as cfeature
 
 # !!! CONTROL VARIABLES !!!
 # bounding box for map [min_longitude, max_longitude, min_latitude, max_latitude]
-extent = [-125, -65, 24, 50]  # United States
+# extent = [-125, -65, 24, 50]  # United States
+# extent for New York and Pennsylvania, zoomed out
+extent = [-82, -72, 38, 45]
 # !!! CONTROL VARIABLES !!!
 
-grace_path1 = 'final-project\\data-small\\GRACEDADM_CLSM025GL_7D.A20030203.030.nc4'
-grace_path2 = 'final-project\\data-small\\GRACEDADM_CLSM025GL_7D.A20221226.030.nc4'
+grace_path1 = 'final-project\\data-penn\\GRACEDADM_CLSM025GL_7D.A20220606.030.nc4'
+grace_path2 = 'final-project\\data-penn\\GRACEDADM_CLSM025GL_7D.A20030602.030.nc4'
+
 
 grace_path = [grace_path1, grace_path2]
 grace = xr.open_mfdataset(grace_path)['gws_inst']
 
 # Compute the difference between the two datasets
-grace_diff = grace.sel(time='2022-12-26') - grace.sel(time='2003-02-03')
+grace_diff = grace.sel(time='2022-06-06') - grace.sel(time='2003-06-02')
 
 
 # Plot the data
 ax = plt.axes(projection=ccrs.PlateCarree())
 ax.set_extent(extent)
 ax.coastlines()
-ax.add_feature(cfeature.STATES, edgecolor='white', linewidth=0.5)
-grace_diff.plot(ax=ax, transform=ccrs.PlateCarree(), cmap='RdBu')
+ax.add_feature(cfeature.STATES, edgecolor='black', linewidth=0.5)
+grace_diff.plot(ax=ax, transform=ccrs.PlateCarree(), cmap='RdBu', cbar_kwargs={'label': 'Groundwater Storage Percentile', 'shrink': 0.6})
+# plt.title('GRACE Groundwater Storage Change from 2022 to 2003, July')
 plt.show()
 
--- final-project/depth.py
+++ final-project/depth.py
@@ -7,13 +7,14 @@ filepath2 = 'final-project\\data-depth\\penn-820.csv'
 filepath3 = 'final-project\\data-depth\\penn-929.csv'
 
 filepaths = [filepath1, filepath2, filepath3]
+labels = ['Pittsburgh', 'Plant 1', 'Plant 2']
 
 fig, ax = plt.subplots()
-for filepath in filepaths:
+for i, filepath in enumerate(filepaths):
     # Load the tab-delimited file into a dataframe
     df = pd.read_csv(filepath, sep='\t')
     df_month6 = df[(df['month_nu'] == 6) & (df['year_nu'] > 2002)]
-    ax.scatter(df_month6['year_nu'], df_month6['mean_va'], label=f'{filepath}')
+    ax.scatter(df_month6['year_nu'], df_month6['mean_va'], label=f'{labels[i]}')
 
     slope, intercept = np.polyfit(df_month6['year_nu'], df_month6['mean_va'], 1)
     ax.plot(df_month6['year_nu'], slope*df_month6['year_nu'] + intercept, '--')
@@ -24,4 +25,7 @@ ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
 
 # Put a legend to the right of the current axis
 ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+# plt.title('Average Depth to Groundwater in Pennsylvania')
+plt.xlabel('Year')
+plt.ylabel('Depth to Groundwater (ft)')
 plt.show()
