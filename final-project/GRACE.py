933ccdc57f3e0562bdaa353d56c4db3761674aaa
John Wilkinson <93553222+wilkyrlx@users.noreply.github.com>
2023-05-04 13:55:15 -0400

    invert GRACE to correct difference sign

--- final-project/GRACE.py
+++ final-project/GRACE.py
@@ -9,12 +9,12 @@ import cartopy.feature as cfeature
 selector = 0                # 0 for New York and Pennsylvania, 1 for Maine and New Hampshire
 # extent = [-125, -65, 24, 50]  # United States
 extent1 = [-82, -72, 38, 45]     # New York and Pennsylvania, zoomed out
-extent2 = [-75.3, -66.8, 42.8, 47.2]     # Maine and New Hampshire, zoomed out
+extent2 = [-75.3, -66.8, 42.4, 47.2]     # Maine and New Hampshire, zoomed out
 extent = [extent1, extent2][selector]
 title_lables = ['NY/PA', 'ME/NH']
 
-grace_path1 = 'final-project\\data-penn\\GRACEDADM_CLSM025GL_7D.A20220606.030.nc4'
-grace_path2 = 'final-project\\data-penn\\GRACEDADM_CLSM025GL_7D.A20030602.030.nc4'
+grace_path1 = 'final-project\\data-penn\\GRACEDADM_CLSM025GL_7D.A20200601.030.nc4'
+grace_path2 = 'final-project\\data-penn\\GRACEDADM_CLSM025GL_7D.A20030609.030.nc4'
 # !!! CONTROL VARIABLES !!!
 
 
@@ -22,7 +22,8 @@ grace_path = [grace_path1, grace_path2]
 grace = xr.open_mfdataset(grace_path)['gws_inst']
 
 # Compute the difference between the two datasets
-grace_diff = grace.sel(time='2022-06-06')  - grace.sel(time='2003-06-02')
+# make negative because we want to see the change in groundwater storage, inversed
+grace_diff = -(grace.sel(time='2020-06-01')  - grace.sel(time='2003-06-09'))
 
 
 # Plot the data
@@ -31,6 +32,6 @@ ax.set_extent(extent)
 ax.coastlines()
 ax.add_feature(cfeature.STATES, edgecolor='black', linewidth=0.5)
 grace_diff.plot(ax=ax, transform=ccrs.PlateCarree(), cmap='RdBu', cbar_kwargs={'label': 'Groundwater Storage Percentile', 'shrink': 0.6})
-plt.title(f'GRACE Groundwater Storage Change {title_lables[selector]} 2003-2022, July')
+plt.title(f'GRACE Groundwater Storage Change {title_lables[selector]} 2003-2020, July')
 plt.show()
 
